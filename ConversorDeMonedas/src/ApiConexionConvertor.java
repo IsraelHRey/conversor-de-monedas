a9cf5284ec0ed5bb72aff32aabac68bb15e08d87
Israel Hernandez <israel.hdzrys66@gmail.com>
2025-12-30 03:37:32 -0600

    Se modifico el enlace creando una variable para que sea modificado por el usuario y se agrego el http client, request y response, ademas se puso try catch con la recomendacion del ide

--- ConversorDeMonedas/src/ApiConexionConvertor.java
+++ ConversorDeMonedas/src/ApiConexionConvertor.java
@@ -1,10 +1,37 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ApiConexionConvertor {
     public static void main(String[] args) {
 
-        HttpClient client = HttpClient.newHttpClient();
-
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://v6.exchangerate-api.com/v6/55a3630b0ce6bdb2f7a8f907/pair/"))
-                .build();
     }
+
+        public String obtenerDatosApi(String monedaBase, String monedaDestino){
+         String direccion = "https://v6.exchangerate-api.com/v6/55a3630b0ce6bdb2f7a8f907/pair/"+monedaBase+"/"
+                 +monedaDestino;
+
+            HttpClient client = HttpClient.newHttpClient();
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+
+            HttpResponse<String> response = null;
+            try {
+                response = client
+                        .send(request, HttpResponse.BodyHandlers.ofString());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            return response.body();
+        }
+
+
+
 }
