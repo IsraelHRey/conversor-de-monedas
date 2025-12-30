14c8744a5457d6701e141b299ac06c626a7e35bf
Israel Hernandez <israel.hdzrys66@gmail.com>
2025-12-30 04:28:56 -0600

    Se creo objeto de clase ApiConexion, se creo una variable llamada datos para llamar a el mto que se hizo e imprimimos la variable datos

--- ConversorDeMonedas/src/Principal.java
+++ ConversorDeMonedas/src/Principal.java
@@ -1,4 +1,12 @@
 public class Principal {
+    public static void main(String[] args) {
+
+        ApiConexionConvertor consultor = new ApiConexionConvertor();
+
+        String datos = consultor.obtenerDatosApi("USD","MXN");
+        System.out.println(datos);
+
+    }
 
 
 
