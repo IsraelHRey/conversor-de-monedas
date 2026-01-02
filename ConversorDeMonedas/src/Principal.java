df4697f8dc0db930c9edc23e7cd8190acd4e69fc
Israel Hernandez <israel.hdzrys66@gmail.com>
2026-01-02 04:00:53 -0600

    Se hizo un metodo para no poner tanto codigo en el swich, se agregaron try catch para letras, se termino

--- ConversorDeMonedas/src/Principal.java
+++ ConversorDeMonedas/src/Principal.java
@@ -1,6 +1,7 @@
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Principal {
@@ -8,67 +9,75 @@ public class Principal {
 
         ApiConexionConvertor consultor = new ApiConexionConvertor();
 
-        String datos = consultor.obtenerDatosApi("USD","MXN");
-        System.out.println(datos);
-
-        JsonObject jsonObject = JsonParser.parseString(datos).getAsJsonObject();
-        double valorMoneda = jsonObject.get("conversion_rate").getAsDouble();
-        System.out.println("El valor de la tasa es: "+valorMoneda);
-
         Scanner lector = new Scanner(System.in);
-        System.out.println("Que cantidad deseas realizar tu conversion?");
-        double cantidad = lector.nextDouble();
-
-        double conversion = valorMoneda*cantidad;
-        System.out.println("La conversion es de " + String.format("%.2f", conversion) + " MXN") ;
 
         int opcion = 0;
 
         while (opcion != 7) {
 
-            System.out.println("******************************************");
-            System.out.println("Sea bienvenido/a al Conversor de Monedas");
-            System.out.println("1) Peso Mexicano ==> Dólar");
-            System.out.println("2) Dólar ==> Peso Mexicano");
-            System.out.println("3) Dólar ==> Real Brasileño");
-            System.out.println("4) Real Brasileño ==> Dólar");
-            System.out.println("5 Dolar ==> peso Argentino");
-            System.out.println("6 Peso Argentino ==> Dolar");
-            System.out.println("7) Salir");
-            System.out.println("Elija una opción válida:");
-            System.out.println("******************************************");
-
-            System.out.println("Elija una opcion");
-            opcion = lector.nextInt();
+            try {
 
 
-            switch (opcion){
-                case 1:
-                    consultor.obtenerDatosApi("MXN", "USD");
-                    break;
-                case 2:
-                    consultor.obtenerDatosApi("USD","MXN");
-                    break;
-                case 3: consultor.obtenerDatosApi("USD","BRL");
-                    break;
-                case 4:consultor.obtenerDatosApi("BRL","USD");
-                    break;
-                case 5: consultor.obtenerDatosApi("USD","ARS");
-                    break;
-                case 6: consultor.obtenerDatosApi("ARS","USD");
-                    break;
-                case 7:
-                    System.out.println("Gracias por usar la aplicacion");
-                    break;
-                default:
-                    System.out.println("Solo se admiten las opciones mencionadas");
-            }
+                System.out.println("\n******************************************\n");
+                System.out.println("Sea bienvenido/a al Conversor de Monedas\n");
+                System.out.println("1) Peso Mexicano ==> Dólar");
+                System.out.println("2) Dólar ==> Peso Mexicano");
+                System.out.println("3) Dólar ==> Real Brasileño");
+                System.out.println("4) Real Brasileño ==> Dólar");
+                System.out.println("5 Dolar ==> peso Argentino");
+                System.out.println("6 Peso Argentino ==> Dolar");
+                System.out.println("7) Salir");
+                System.out.println("Elija una opción válida:\n");
+                System.out.println("******************************************\n");
+                System.out.println("Elija una opcion");
+
+                opcion = lector.nextInt();
+
+
+                switch (opcion) {
+                    case 1:
+                        realizarConversion("MXN", "USD", consultor, lector);
+                        break;
+                    case 2:
+                        realizarConversion("USD", "MXN", consultor, lector);
+                        break;
+                    case 3:
+                        realizarConversion("USD", "BRL", consultor, lector);
+                        break;
+                    case 4:
+                        realizarConversion("BRL", "USD", consultor, lector);
+                        break;
+                    case 5:
+                        realizarConversion("USD", "ARS", consultor, lector);
+                        break;
+                    case 6:
+                        realizarConversion("ARS", "USD", consultor, lector);
+                        break;
+                    case 7:
+                        System.out.println("Gracias por usar la aplicacion");
+                        break;
+                    default:
+                        System.out.println("Solo se admiten las opciones mencionadas");
+                }
+            } catch (InputMismatchException e){System.out.println("\n Error: Elija una opción válida (solo números del 1 al 7).");
+                lector.next();}
 
         }
      }
 
     public static void realizarConversion(String base, String destino, ApiConexionConvertor consultor, Scanner lector) {
-        String datos = consultor.obtenerDatosApi(base, destino);
+        try {
+            String datos = consultor.obtenerDatosApi(base, destino);
+            JsonObject jsonObject = JsonParser.parseString(datos).getAsJsonObject();
+            double valorMoneda = jsonObject.get("conversion_rate").getAsDouble();
+            System.out.println("Que cantidad deseas realizar tu conversion?\n");
+            double cantidad = lector.nextDouble();
+            double conversion = valorMoneda * cantidad;
+            System.out.println("\nLa conversion es de " + String.format("%.2f", conversion));
+        }catch (Exception e){
+            System.out.println("\nERROR: Por favor, ingresa solo números.");
+            lector.nextLine();
+        }
 
     }
 
